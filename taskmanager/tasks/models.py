21b54760d25d9642c605c7a5b6a16931c3472bf6
yuusaif <alvi676@gmail.com>
2025-06-17 02:08:10 +0600

    New: model and serializer for tasks app

--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
